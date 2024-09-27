9dc2fa0a964a55e277549263883415b8698ad684
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2024-09-27 15:21:55 +0000

    adding drawers to products, customers and orders

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,13 +1,26 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
+import React, {useState} from 'react'
 
-import Toolbar from '@mui/material/Toolbar';
 
-import Typography from '@mui/material/Typography';
+import {
+    Typography,
+    Container,
+    Button,
+    Toolbar,
+    AppBar
+} from '@mui/material/';
 
-import Container from '@mui/material/Container';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
-import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonIcon from '@mui/icons-material/Person';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import AddIcon from '@mui/icons-material/Add';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
@@ -15,7 +28,98 @@ import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 const Header = () => {
 
 
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const [selectedButton, setSelectedButton] = useState('')
+
+  const handleToggleMenu = (selected) => {
+    setMenuOpen(!menuOpen)
+    setSelectedButton(selected)
+    
+  }
+
+  const renderMenu = () => {
+
+    switch (selectedButton) {
+        case 'products':
+            
+            return (
+                <List>
+                    <ListItem button > 
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText>Home</ListItemText>         
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <InventoryIcon />
+                        </ListItemIcon>
+                        <ListItemText>Products</ListItemText>         
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <AddIcon />
+                        </ListItemIcon>
+                        <ListItemText>Add Product</ListItemText>         
+                    </ListItem>
+                </List>
+            );
+        
+    
+        case 'customers':
+
+            return (
+                <List>
+                    <ListItem button > 
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText>Home</ListItemText>         
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <PersonIcon />
+                        </ListItemIcon>
+                        <ListItemText>Customers</ListItemText>         
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <PersonAddIcon />
+                        </ListItemIcon>
+                        <ListItemText>Add Customer</ListItemText>         
+                    </ListItem>
+                </List>
+            );
+        
+        case 'orders': 
+
+            return (
+                <List>
+                    <ListItem button > 
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText>Home</ListItemText>         
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemIcon>
+                            <ShoppingCartIcon />
+                        </ListItemIcon>
+                        <ListItemText>Orders</ListItemText>         
+                    </ListItem>               
+                </List>
+            );
+        
+        default: return('')
+    }
+  } 
+
+
   return (
+    <>
+    
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
@@ -40,7 +144,7 @@ const Header = () => {
 
           <Button
                 
-            
+            onClick={() => handleToggleMenu('products')}
             sx={{ mr: 2, my: 2, color: 'white', display: 'block' }}
           >
             Products
@@ -48,7 +152,7 @@ const Header = () => {
 
           <Button
                 
-            
+            onClick={() => handleToggleMenu('customers')}
             sx={{ mr: 2, my: 2, color: 'white', display: 'block' }}
           >
             Customers
@@ -56,7 +160,7 @@ const Header = () => {
 
           <Button
                 
-            
+            onClick={() => handleToggleMenu('orders')}
             sx={{ mr: 2, my: 2, color: 'white', display: 'block' }}
           >
             Orders
@@ -65,6 +169,10 @@ const Header = () => {
         </Toolbar>
       </Container>
     </AppBar>
+    <Drawer open={menuOpen} onClose={() => handleToggleMenu()}>
+        {renderMenu()}
+    </Drawer>
+    </>
   );
 }
 export default Header;
